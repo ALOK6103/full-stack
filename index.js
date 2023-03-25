0e09d6c540d1624bc3a55a9e7da58717b57348d0
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-03-25 17:13:47 +0530

    added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/users",userRouter)
 app.use(auth)
 app.use("/notes",noteRouter)
 
-app.listen(process.env.PORT,async()=>{
+app.listen(process.env.port,async()=>{
     try {
         await connection
         console.log("connected")
